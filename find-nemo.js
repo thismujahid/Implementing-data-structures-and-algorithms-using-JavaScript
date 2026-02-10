30b1e16da8031f3956449d9b3bd5aa8fa4a947cf
Mohamed Mujahid <Mohamed.mojahead@gmail.com>
2026-02-10 18:36:10 +0200

    Update find-nemo.js

--- find-nemo.js
+++ find-nemo.js
@@ -1,12 +1,14 @@
 const fishName = 'Nemo';
 const fishNames = ['Nemo','Hemo','Bremo','Kemo','Nemo'];
-const largeArr = new Array(1000).fill('nemo');
-
-let t0 = performance.now();
-for (let index = 0; index < largeArr.length; index++) {
-    const element = largeArr[index];
-    if(element === 'nemo'){
-        console.log('Nemo Found')
+function findNemo(arr){
+    let t0 = performance.now();
+    for (let index = 0; index < arr.length; index++) {
+        const element = arr[index];
+        if(element === fishName){
+            console.log(fishName,'Found')
+        }
     }
+    console.log('Search Took: ',(performance.now()- t0).toFixed(2),'ms')
 }
-console.log('Search Took: ',(performance.now()- t0).toFixed(2),'ms')
+
+findNemo(fishNames) // O(n)
